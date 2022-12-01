c2d666cf2e8307f989299f53cfc393535ff57948
newsyd04 <newsyd04@gmail.com>
2022-12-01 11:01:26 +0000

    Newest Version With Password Authentication

---
+++ src/passwordAuthentication.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+public class passwordAuthentication{
+    static String[] UserID = {"1223", "2334", "3445"};
+    static String[] passwords = {"student123", "ta123", "lecturer123"};
+    static String enteredUserID = "20000.3241476520292563523203147596842563301524627543562918413124165976451834652";
+    static String enteredPassword = "student123";
+    static boolean found = false;
+    boolean success = false;
+    public static boolean password() {
+        int index = -1;
+        for (int i=0;i<UserID.length;i++) {
+            if (UserID[i].equals(enteredUserID)) {
+                index = i;
+                break;
+            }
+            else{
+                return false;
+            }
+        }
+
+        if(passwords[index] == enteredPassword){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(password());
+
+
+    }
+}
