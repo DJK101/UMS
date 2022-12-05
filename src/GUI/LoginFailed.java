ce7df2818f63fe4a1daadecff45f065eb4843181
Rosie_Kennelly <22340238@studentmail.ul.ie>
2022-12-05 09:54:39 +0000

    you can now run the gui from the class

--- src/GUI/LoginFailed.java
+++ src/GUI/LoginFailed.java
@@ -2,7 +2,19 @@ package GUI;
 
 import javax.swing.*;
 
-public class LoginFailed {
+public class LoginFailed extends JFrame{
     private JLabel lblLogin;
     private JButton tryAgainButton;
+    private JPanel LoginFailedBckgrd;
+
+    public LoginFailed() {
+        setContentPane(LoginFailedBckgrd);
+        setSize(1550,850);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        LoginFailed myFrame = new LoginFailed();
+    }
 }
