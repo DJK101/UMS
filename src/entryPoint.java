3c42aad5819dc1d92059256b48202e7090bc5686
Daniel J Kennedy <danieljeffersonkennedy@gmail.com>
2022-12-06 10:54:12 +0000

    Removed import statements from entryPoint

--- src/entryPoint.java
+++ src/entryPoint.java
@@ -1,8 +1,3 @@
-import Person.Lecturer;
-import Person.President;
-import Person.Student;
-import Person.TA;
-
 public class entryPoint {
     public static void main(String[] args) {
         new LoginGUI(); // Currently prints inputs to console
