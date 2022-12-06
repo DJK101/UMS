5f2cd7d3786bf60988383098c9db3dc84b0a0ee4
Daniel J Kennedy <danieljeffersonkennedy@gmail.com>
2022-12-06 12:17:27 +0000

    Added comments to PasswordAuthentication to make it more readable

--- src/PasswordAuthentication.java
+++ src/PasswordAuthentication.java
@@ -4,12 +4,15 @@ public class PasswordAuthentication {
 
         int index = 0;
 
+        //Loops through the ids that exist in the system
         for (int i = 0; i < PremadeUsers.getIds().length; i++) {
+            //If the id is found, saves the index of that id to index
             if (PremadeUsers.getIds()[i].equals(enteredUserID)) {
                 index = i;
                 break;
             }
         }
+        //Will return true iff id and password match
         return PremadeUsers.getPwds()[index].equals(enteredPassword);
     }
 }
