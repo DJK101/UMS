99a4ad5ddea3c19f349590881773f633baba10af
Daniel J Kennedy <danieljeffersonkennedy@gmail.com>
2022-12-06 11:37:04 +0000

    removed unused String arrays in passwordAuthentication

--- src/passwordAuthentication.java
+++ src/passwordAuthentication.java
@@ -1,8 +1,5 @@
 public class passwordAuthentication {
 
-    private static final String[] UserID = {"1", "1223", "2334", "3445"};
-    private static final String[] passwords = {"pwd", "student123", "ta123", "lecturer123"};
-
     public static boolean password(String enteredUserID, String enteredPassword) {
 
         int index = 0;
