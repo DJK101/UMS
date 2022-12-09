838f88d43ef6a2e1a8bca145127f07b94604e747
Rosie_Kennelly <22340238@studentmail.ul.ie>
2022-12-09 16:06:20 +0000

    made a background for external area

--- src/gui/ExternalArea.java
+++ src/gui/ExternalArea.java
@@ -3,5 +3,14 @@ package gui;
 import javax.swing.*;
 
 public class ExternalArea {
-    private JLabel noodleLogo;
+
+    private JPanel externalArea;
+
+    public static void main(String[] args) {
+
+        LoginFailed myFrame = new LoginFailed();
+        JFrame f=new JFrame();
+        f.add(myFrame);
+        f.setVisible(true);
+    }
 }
