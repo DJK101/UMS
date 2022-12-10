ff702aa9e76eab0e01ee2ccb2ba8a38eac4cc78d
DJK101 <danieljeffersonkennedy@gmail.com>
2022-12-10 23:45:27 +0000

    Added login functionality to ExternalArea

--- src/gui/ExternalArea.java
+++ src/gui/ExternalArea.java
@@ -1,16 +1,20 @@
 package gui;
 
+import data.PasswordAuthentication;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ExternalArea extends JFrame {
+public class ExternalArea extends JFrame implements ActionListener {
 
     private JPanel externalAreaBg;
     private JLabel noodleLogo;
     private JLabel lblLogin;
     private JTextField textField1;
     private JPasswordField passwordField1;
-    private JButton button1;
+    private JButton submitButton;
 
     public ExternalArea() {
 
@@ -21,6 +25,8 @@ public class ExternalArea extends JFrame {
         icon = new ImageIcon(new_img);
         noodleLogo.setIcon(icon);
 
+        submitButton.addActionListener(this);
+
         //Create and set up the window
         setTitle("Noodle - Login");
         setIconImage(icon.getImage());
@@ -28,5 +34,15 @@ public class ExternalArea extends JFrame {
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == submitButton && PasswordAuthentication.password(textField1.getText(), passwordField1.getText())) {
+            new InternalArea(textField1.getText());
+        } else {
+            new LoginFailed();
+        }
+        dispose();
     }
 }
